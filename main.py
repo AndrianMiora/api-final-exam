b80a0020f62c8d11c9b23253cfc142cda33cde3d
Miora Andrian <miora.andri@icloud.com>
2025-09-03 11:34:07 +0300

    feat: part_one: FastAPI - HTTP methods

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.requests import Request
-from starlette.responses import JSONResponse
+from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
+
+@app.get('/ping')
+def read_ping ():
+    return Response
+
+@app.post('/cars')
+
+@app.get("/cars")
+
+@app.get('/cars/{id}')
+
+@app.put('/cars/{id}/characteristics')
